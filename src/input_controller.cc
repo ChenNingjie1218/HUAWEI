98c300310abe57a8d5c1c3927c99ee986e41c569
Jokerjay <578055568@qq.com>
2024-03-21 14:43:21 +0800

    增加船容量修改参数，可以改变船的节奏

--- src/input_controller.cc
+++ src/input_controller.cc
@@ -100,6 +100,7 @@ void InputController::Init() {
   }
   // 船容积
   scanf("%d", &Boat::boat_capacity);
+  Boat::boat_capacity -= BOAT_CAPACITY_REDUCE;
 #ifdef DEBUG
   fprintf(debug_map_file, "船容量：%d\n", Boat::boat_capacity);
 #endif
