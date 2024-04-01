2ba3270a6fc3b13810b5c2751c2c47ea6c287dcf
Jokerjay <578055568@qq.com>
2024-04-01 14:27:12 +0800

    修改map头文件

--- include/map_controller.h
+++ include/map_controller.h
@@ -5,6 +5,7 @@
 #include <vector>
 
 #include "astar.h"
+#include "berth.h"
 #include "goods.h"
 struct MapController {
  private:
@@ -15,16 +16,30 @@ struct MapController {
  public:
   static MapController *&GetInstance();
 
-  int parent[N * N];
-
-  Goods *gds[N][N] = {{nullptr}};
-  char ch[N][N];
+  int parent[N * N];               // 并查集区域
+  int busy_point[N][N];            // 堵车点
+  Goods *gds[N][N] = {{nullptr}};  // 货物地图
+  Berth berth[berth_num + 10];     // 泊位数据
+  /*
+   * - · 空地
+   * - * 海洋
+   * - # 障碍
+   * - A 机器人起始位置，总共10个
+   * - B 大小为4*4，标识泊位的位置
+   */
+  char ch[N][N];  // 地图数据
   std::vector<std::pair<int, int>> robot_purchase_point;
   std::vector<std::pair<int, int>> boat_purchase_point;
   std::vector<std::pair<int, int>> delivery_point;
 
   // 坐标映射到泊位id
   std::map<Location, int> location_to_berth_id;
+
+  // 机器人可达
+  bool CanRobotReach(int x, int y);
+
+  // 初始化坐标映射到泊位id的map
+  void InitBerthMap(int berth_id, int berth_x, int berth_y);
 
   // 用并查集分区
   // 找集合
