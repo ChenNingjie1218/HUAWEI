8b5db801054c9020d7ce5c18fcb5e3c5ba9cc4f9
Jokerjay <578055568@qq.com>
2024-04-01 15:44:41 +0800

    恢复main

--- src/main.cc
+++ src/main.cc
@@ -24,35 +24,24 @@ int main() {
   dup2(output_fd, STDERR_FILENO);
 #endif
   InputController::GetInstance()->Init();
-  // for (int i = 0; i < 15000; ++i) {
-  //   // #ifdef DEBUG
-  //   //     if (i == 1) fclose(debug_command_file);
-  //   // #endif
-  //   InputController::GetInstance()->Input();
-  //   // --------- 准备阶段 ----------
-  //   DecisionManager::GetInstance()->ClearQueue();  // 清空决策队列
+  for (int i = 0; i < 15000; ++i) {
+    InputController::GetInstance()->Input();
+    // --------- 准备阶段 ----------
+    DecisionManager::GetInstance()->ClearQueue();  // 清空决策队列
 
-  //   // --------- 决策阶段 ----------
-  //   DecisionManager::GetInstance()->DecisionRobot();
-  //   DecisionManager::GetInstance()->DecisionBoat();
+    // --------- 决策阶段 ----------
+    DecisionManager::GetInstance()->DecisionRobot();
+    DecisionManager::GetInstance()->DecisionBoat();
 
-  //   // --------- 输出阶段 ----------
-  //   OutputController::GetInstance()->Output();
-  // }
+    // --------- 输出阶段 ----------
+    OutputController::GetInstance()->Output();
+  }
 #ifdef DEBUG
   fclose(debug_command_file);
   std::vector<Berth> &berth = MapController::GetInstance()->berth;
   // 泊位上残留货物数量
   for (int i = 0; i < 10; ++i) {
     std::cerr << "船上残留货物:" << berth[i].goods_num << std::endl;
-  }
-
-  // 地上残留货物
-  Goods *head_goods = GoodsManager::GetInstance()->head_goods;
-  Goods *cur = head_goods->next;
-  while (cur != head_goods) {
-    std::cerr << "地上残留货物: " << cur->money << std::endl;
-    cur = cur->next;
   }
 #endif
   return 0;
