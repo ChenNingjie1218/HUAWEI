bbc6ab6ad5d40e28cbc2cde85274fe6db10bbef2
Jokerjay <578055568@qq.com>
2024-04-01 15:23:43 +0800

    租赁模块简单实现

---
+++ src/rent_controller.cc
@@ -0,0 +1,15 @@
+#include "rent_controller.h"
+RentController* RentController::instance_ = nullptr;
+
+RentController*& RentController::GetInstance() {
+  if (!instance_) {
+    instance_ = new RentController();
+  }
+  return instance_;
+}
+
+// 租船
+void RentController::RentBoat(int x, int y) { boat.push_back(Boat(x, y)); }
+
+// 租机器人
+void RentController::RentRobot(int x, int y) { robot.push_back(Robot(x, y)); }
