8e35dd646310bd73ed03182b4c11e9dad2c34935
Jokerjay <578055568@qq.com>
2024-04-09 17:58:31 +0800

    增加随机获取泊位坐标的方法

--- src/berth.cc
+++ src/berth.cc
@@ -7,3 +7,19 @@ Berth::Berth(int id, int x, int y, int loading_speed)
   goods_num = 0;
   area_id = MapController::GetInstance()->FindArea(x * n + y, false);
 }
+
+// 获取该泊位的随机x坐标
+int Berth::GetRandomX() {
+  if (loc_index == static_cast<int>(loc.size())) {
+    loc_index = 0;
+  }
+  return loc[loc_index++].x;
+}
+
+// 获取该泊位的随机y坐标
+int Berth::GetRandomY() {
+  if (loc_index == static_cast<int>(loc.size())) {
+    loc_index = 0;
+  }
+  return loc[loc_index++].y;
+}
